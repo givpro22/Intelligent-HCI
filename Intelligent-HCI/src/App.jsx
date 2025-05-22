d13a2b70171e7f98c845fd41a46d406a1212c1df
Young Seo <givpro22@daum.net>
2025-05-22 15:39:43 +0900

    First Commit

---
+++ Intelligent-HCI/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function App() {
+  return (
+    <h1 className="text-3xl bg-violet-500 font-bold underline">
+      Hello world!
+    </h1>  )
+}
+
+export default App
